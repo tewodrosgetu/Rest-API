186e4fc13c86431c517385b4bbf33a98e6c119e7
tewodrosgetu <tewodrosgetu18@gmail.com>
2025-07-23 03:35:15 -0800

    validation server side ended

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 exports.getPost = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -15,10 +17,24 @@ exports.getPost = (req, res, next) => {
   });
 };
 exports.postPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "validation is failed ,entered incorrecte data",
+      errors: errors.array(),
+    });
+  }
   const title = req.body.title;
   const content = req.body.content;
   res.status(201).json({
     message: "post created successfuly!",
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "teda" },
+      createdAt: new Date(),
+    },
   });
 };
