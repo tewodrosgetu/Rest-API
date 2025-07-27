ceab640d9f5b72f0621773dead0231d01a380903
tewodrosgetu <tewodrosgetu18@gmail.com>
2025-07-27 03:48:17 -0800

    usersign up ended

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const { validationResult } = require("express-validator");
-
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 exports.signUp = (req, res, next) => {
@@ -13,4 +13,23 @@ exports.signUp = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const name = req.body.name;
+  bcrypt
+    .hash(password, 12)
+    .then((hashpassword) => {
+      const user = new User({
+        name: name,
+        email: email,
+        password: hashpassword,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({ message: "user created", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.status = 500;
+      }
+      next(err);
+    });
 };
