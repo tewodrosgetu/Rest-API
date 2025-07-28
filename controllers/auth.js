70bcba9312369aa43c4cc7f0b2d4c5d833c4edc3
tewodrosgetu <tewodrosgetu18@gmail.com>
2025-07-28 04:21:25 -0800

    status update ended

--- controllers/auth.js
+++ controllers/auth.js
@@ -73,3 +73,42 @@ exports.login = (req, res, next) => {
       next(err);
     });
 };
+exports.getUserStatus = (req, res, next) => {
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error("user not found");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ status: user.status });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.status = 500;
+      }
+      next(err);
+    });
+};
+exports.upDateUserStatus = (req, res, next) => {
+  const newStatus = req.body.status;
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error("user not found");
+        error.statusCode = 404;
+        throw error;
+      }
+      user.status = newStatus;
+      return user.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "updated the status" });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.status = 500;
+      }
+      next(err);
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { body } = require("express-validator");
 const User = require("../models/user");
 const authController = require("../controllers/auth");
+const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
 router.put(
@@ -24,5 +25,12 @@ router.put(
   authController.signUp
 );
 router.post("/login", authController.login);
+router.get("/status", isAuth, authController.getUserStatus);
+router.put(
+  "/status",
+  isAuth,
+  [body("status").trim().not().isEmpty()],
+  authController.upDateUserStatus
+);
 
 module.exports = router;
